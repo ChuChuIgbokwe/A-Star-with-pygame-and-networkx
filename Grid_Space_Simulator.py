9d33aaf6cad25109af7e0dac244b65ac81271a5f
ChuChuIgbokwe <ChuChuIgbokwe@users.noreply.github.com>
2017-02-09 15:46:19 -0600

    Screen Instructions Added

--- Grid_Space_Simulator.py
+++ Grid_Space_Simulator.py
@@ -8,6 +8,7 @@ import pygame as pg
 
 class GridSpaceSimulator:
     def __init__(self,width=600,height=600):
+        pg.init()
         self.width = width
         self.height = height
         self.cell_size = (20,20)
@@ -16,22 +17,27 @@ class GridSpaceSimulator:
         self.G = self._2d_grid()
         self.clock = pg.time.Clock()
         self.fps = 60
+        pg.display.set_caption("Pathfinder")
+        self.myfont = pg.font.SysFont('arial', 15)
         self.obstacle_weight = 1000
         self.start_cell = None
         self.goal_cell = None
         self.barrier_cell = None
         self.barriers = []
+        self.path = None
         #Flags
         self.start_flag = False
         self.goal_flag = False
         self.obstacle_flag = False
         self.solved = False
+        self.path_found = False
         #colours
         self.WHITE = (255, 255, 255)
         self.BLACK = (0, 0, 0)
         self.RED = (255, 0, 0)
         self.GREEN = (0, 255, 0)
         self.BLUE = (0, 0, 255)
+        self.ORANGE = (255, 175, 0)
 
 
     def initBoard(self,board):
@@ -48,6 +54,10 @@ class GridSpaceSimulator:
         return self.background
 
     def _2d_grid(self):
+        '''
+        create 2D weighted grid graph
+        :return:
+        '''
         Graph = nx.Graph()
         no_vertical_lines = ((self.width - 2 * self.cell_size[0]) / 20) + 1
         no_horizontal_lines = ((self.height - 2 * self.cell_size[1]) / 20) + 1
@@ -59,21 +69,37 @@ class GridSpaceSimulator:
         return Graph
 
     def _get_target(self):
-        # User clicks the mouse. Get the position
-        self.mouse = pg.mouse.get_pos()
+        '''
+        Get mouse position when clicked and convert it to grid coordinates
+        :return:
+        '''
+        self.mouse = pg.mouse.get_pos() # User clicks the mouse. Get the position
         # Change the x/y screen coordinates to grid coordinates
         column = self.mouse[0] // self.cell_size[0] - 1
         row = self.mouse[1] // self.cell_size[1] - 1
         return (row, column)
 
-    def _set_barrier_weights(self):
-        barrier_node = self._get_target()
-        for i in self.G.edges(barrier_node):
+    def _set_barrier_weights(self,cell):
+        '''
+        set the weight of all edges of an obstacle node to
+        :param cell:
+        :return:
+        '''
+        for i in self.G.edges(cell):
             self.G.edge[i[0]][i[1]]['weight'] = self.obstacle_weight
 
+    def _dist(self,a, b):
+        (x1, y1) = a
+        (x2, y2) = b
+        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
     def _draw_cell(self,colour):
+        '''
+        Get mouse click, convert it to grid coordinates then fill it out with the appropriat colour
+        :param colour:
+        :return:
+        '''
         self.mouse = pg.mouse.get_pos()
-        # Change the x/y screen coordinates to grid coordinates
         column = self.mouse[0] // self.cell_size[0] - 1
         row = self.mouse[1] // self.cell_size[1] - 1
         p_column = (column + 1) * self.cell_size[0]
@@ -82,78 +108,131 @@ class GridSpaceSimulator:
         return (row, column)
 
     def _clear_cell(self,cell):
+        '''
+        Clear out a filled grid. Used to reset the simulator
+        :param cell:
+        :return:
+        '''
         p_column = (cell[0] + 1) * self.cell_size[0]
         p_row = (cell[1] + 1) * self.cell_size[0]
         pg.draw.rect(self.screen, self.BLACK, (p_row, p_column, self.cell_size[0], self.cell_size[1]))
 
     def _draw_path(self,cell):
+        '''
+        Convert nodes to pixel coordinates and fill out the appropriate grid squares. ITht's Used for drawing the A star path on the simulator
+        :param cell:
+        :return:
+        '''
         p_column = (cell[0] + 1) * self.cell_size[0]
         p_row = (cell[1] + 1) * self.cell_size[0]
         pg.draw.rect(self.screen, self.WHITE, (p_row, p_column, self.cell_size[0], self.cell_size[1]))
+
+    def _whipeout(self,cell):
+        '''
+        Convert nodes to pixel coordinates and fill out the appropriate grid squares. ITht's Used for drawing the A star path on the simulator
+        :param cell:
+        :return:
+        '''
+        p_column = (cell[0] + 1) * self.cell_size[0]
+        p_row = (cell[1] + 1) * self.cell_size[0]
+        pg.draw.rect(self.screen, self.ORANGE, (p_row, p_column, self.cell_size[0], self.cell_size[1]))
+
+
+    def _onscreen_instructions(self):
+        if not self.start_flag and not self.solved:
+            write1 = self.myfont.render("Place your start point", True, (255, 255, 255))
+            self.screen.blit(write1, (10, 1))
+        if self.start_flag and not self.goal_flag:
+            write2 = self.myfont.render("Place your end point ", True, (255, 255, 255))
+            self.screen.blit(write2, (10, 1))
+
+        if self.start_flag and self.goal_flag:
+            write3 = self.myfont.render("Add obstacles or press SPACEBAR to solve", 1, (255, 255, 255))
+            self.screen.blit(write3, (10, 1))
+
+        if self.solved:
+            write4 = self.myfont.render("Press ENTER to restart simulator", 1, (255, 255, 255))
+            self.screen.blit(write4, (10, 1))
+            write5 = self.myfont.render("Press R to remove obstacles", 1, (255, 255, 255))
+            self.screen.blit(write5, (1, self.height-self.cell_size[0]))
+
+        if self.solved and  not self.path_found:
+            write6 = self.myfont.render("No Path Found", 1, (255, 255, 255))
+            self.screen.blit(write6, (450,1))
+        if self.solved and self.path_found:
+            write6 = self.myfont.render("Path Found", 1, (255, 255, 255))
+            self.screen.blit(write6, (450, 1))
+            write6 = self.myfont.render("Steps:{}".format(len(self.path)-1), 1, (255, 255, 255))
+            self.screen.blit(write6, (450, self.height-self.cell_size[0]))
 
     def run(self):
         board = self.initBoard(self.screen)
         done = False
         while not done:
-            self.clock.tick(self.fps)
-            pg.display.update()
             pos = pg.mouse.get_pos()
             for event in pg.event.get():
                 self.screen.blit(board, (0, 0))
-                pg.display.flip()
+                self._onscreen_instructions()
+
                 if event.type == pg.QUIT:
                     done = True
 
                 elif event.type == pg.MOUSEBUTTONDOWN and pg.Rect(20, 20, self.width - self.cell_size[0] * 2, self.height - self.cell_size[1] * 2).collidepoint(
                         pos) and self.start_flag == False and event.button == 1 and self.solved == False:
                     self.start_cell = self._draw_cell(self.RED)
-                    print 'start cell ', self.start_cell
+                    # print 'start cell ', self.start_cell
                     self.start_flag = True
+
                 elif event.type == pg.MOUSEBUTTONDOWN and pg.Rect(20, 20, self.width - self.cell_size[0] * 2, self.height - self.cell_size[1] * 2).collidepoint(
                         pos) and self.goal_flag == False and event.button == 1 and self.solved == False:
-                    self.goal_cell = self._get_target()
 
-                    if self.goal_cell == self.start_cell:
-                        print "Nope"
+                    self.goal_cell = self._get_target()
+                    if self.goal_cell == self.start_cell: #start cell cannot be goal cell
                         self.goal_cell == None
                     else:
                         self.goal_cell = self._draw_cell(self.GREEN)
-                        print 'goal cell ', self.goal_cell
+                        # print 'goal cell ', self.goal_cell
                         self.goal_flag = True
 
                 elif event.type == pg.MOUSEBUTTONDOWN and pg.Rect(20, 20, self.width - self.cell_size[0] * 2, self.height - self.cell_size[1] * 2).collidepoint(
                         pos) and self.start_flag == True and self.goal_flag == True and event.button == 1 and self.solved == False:
+
                     self.barrier_cell = self._get_target()
-                    if self.barrier_cell == self.start_cell or self.barrier_cell == self.goal_cell:
+                    if self.barrier_cell == self.start_cell or self.barrier_cell == self.goal_cell: #no obstacles in star or goal cell
                         self.barrier_cell = None
                     else:
                         self.barrier_cell = self._draw_cell(self.BLUE)
-                        print 'barrier cell ', self.barrier_cell
-                        # print type(barrier_cell)
+                        # print 'barrier cell ', self.barrier_cell
                         self.barriers.append(self.barrier_cell)
-                        # obstacle_flag = True
 
                 elif event.type == pg.KEYDOWN and event.key == pg.K_SPACE and self.start_flag == True and self.goal_flag == True and self.solved == False:
-                    obstacle_flag = True
+                    self.obstacle_flag = True
                     for i in self.barriers:
                         self._set_barrier_weights(i)
-                    check = nx.has_path(self.G, self.start_cell, self.goal_cell)
-                    print self.G.edges(data=True)
-                    # print check
+                    check = nx.has_path(self.G, self.start_cell, self.goal_cell) # check path exists
                     if check:
-                        path = nx.astar_path(self.G, self.start_cell, self.goal_cell)
-                        if set(path).intersection(self.barriers) == set():
-
-                            # print path
-                            for i in path:
-                                if i == path[0] or i == path[-1]:
+                        self.path = nx.astar_path(self.G, self.start_cell, self.goal_cell,heuristic=self._dist)
+                        if set(self.path).intersection(self.barriers) == set():
+                            self.path_found = True
+                            print "A Star path = ", self.path
+                            for i in self.path:
+                                if i == self.path[0] or i == self.path[-1]: #Don't draw on start or goal
                                     pass
                                 else:
                                     self._draw_path(i)
                         else:
-                            print "No path found"
+                            print "NO PATH FOUND"
+                            exempt = self.barriers + [self.start_cell] + [self.goal_cell]
+                            for i in self.G.nodes():
+                                if i not in exempt:
+                                    self._whipeout(i)
+
                     else:
-                        print "No path found"
+                        print "NO PATH FOUND"
+                        exempt = self.barriers + [self.start_cell] + [self.goal_cell]
+                        for i in self.G.nodes():
+                            if i not in exempt:
+                                self._whipeout(i)
 
                     self.start_flag = False
                     self.goal_flag = False
@@ -161,25 +240,30 @@ class GridSpaceSimulator:
                     self.solved = True
 
                 elif event.type == pg.KEYDOWN and event.key == pg.K_RETURN and self.solved == True:
-                    to_be_cleared = self.barriers + path
-                    for i in to_be_cleared:
+                    for i in self.G.nodes():
                         self._clear_cell(i)
                     self.start_cell = self.goal_cell = self.barrier_cell = None
                     self.barriers = []
                     self.solved = False
-                    path = None
+                    self.path = None
+                    print 'SIMULATOR RESTARTED'
 
                 elif event.type == pg.KEYDOWN and event.key == pg.K_r and self.solved == True:
-                    to_be_cleared = self.barriers + path[1:-1]
-                    for i in to_be_cleared:
-                        self._clear_cell(i)
+                    exempt = [self.start_cell] + [self.goal_cell]
+                    for i in self.G.nodes():
+                        if i in exempt:
+                            pass
+                        else:
+                            self._clear_cell(i)
                     self.barriers = None
                     self.barriers = []
                     self.start_flag = self.goal_flag = True
                     self.solved = False
-                    path = None
+                    self.path = None
+                    print 'OBSTACLES REMOVED'
+            pg.display.flip()
+            self.clock.tick(self.fps)
         pg.quit()
-
 
 x = GridSpaceSimulator()
 x.run()
